a168f06b3724ded9ba94b60efa46ea0ba7f2c4a4
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-04-04 14:36:55 +0530

    [Nageshwar] : updated front permitted url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   gatewayUrl: 'https://quantitymeasurementapp-production-e4e2.up.railway.app/', // Your real server
   googleClientId: '572314407201-7u2slqlg4137j7ke26n2qqte517lat7c.apps.googleusercontent.com',
-  googleRedirectUri: 'https://quantitymeasurementapp-production-e4e2.up.railway.app/auth'
+  googleRedirectUri: 'https://quantitymeasurementapp-frontend1.vercel.app'
 };
