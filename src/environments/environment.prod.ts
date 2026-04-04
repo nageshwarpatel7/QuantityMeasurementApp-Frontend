d2aad9c0df36c4c8182f848bc61986d54b2a3191
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-04-04 14:11:59 +0530

    [Nageshwar] : updated front permitted url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'https://api.yourdomain.com', // Your real server
+  gatewayUrl: 'https://quantitymeasurementapp-production-e4e2.up.railway.app/', // Your real server
   googleClientId: '572314407201-7u2slqlg4137j7ke26n2qqte517lat7c.apps.googleusercontent.com',
   googleRedirectUri: 'https://yourdomain.com/auth'
 };
