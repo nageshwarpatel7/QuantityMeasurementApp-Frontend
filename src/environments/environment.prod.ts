28aa2aa65c859e1064ee7c06ed6c22111bdff8e1
Nageshwar Prashad Patel <nageshwarpatel660@gmail.com>
2026-04-04 11:02:01 +0530

    [Nageshwar] : updated minor issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   gatewayUrl: 'https://api.yourdomain.com', // Your real server
-  googleClientId: 'your-production-google-id',
+  googleClientId: '572314407201-7u2slqlg4137j7ke26n2qqte517lat7c.apps.googleusercontent.com',
   googleRedirectUri: 'https://yourdomain.com/auth'
 };
